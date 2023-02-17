b24e4c479cd14a06f8f43defab400e796b297af2
Klementiev Dmitry <109908018+klementievdmitry@users.noreply.github.com>
2023-02-17 21:46:36 +0500

    Plugins keymaps moved from `lua/keymaps.lua` to `after/plugin/*.rc.lua`

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -1,3 +1,5 @@
 local fb_actions = require("telescope").extensions.file_browser.actions
 
 require("telescope").load_extension("file_browser")
+
+vim.keymap.set("n", "<Leader>fb", ":Telescope file_browser<CR>", { silent = true })
