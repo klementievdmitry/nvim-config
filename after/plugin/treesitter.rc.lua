4359cf3413a831c04d728e8cb359183906fa5cfd
Klementiev Dmitry <109908018+klementievdmitry@users.noreply.github.com>
2023-02-17 21:52:26 +0500

    Setup `treesitter` and `telescope`

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -8,5 +8,10 @@ require("nvim-treesitter.configs").setup({
                 "help",
                 "cmake",
         },
+        sync_install = true,
+        auto_install = true,
+        highlight = {
+                enable = true,
+        },
 })
  
